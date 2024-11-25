cfeb7332b933e8b4fec94641049d8d41291364d3
YaKaZi85 <aylimak@gmail.com>
2024-11-25 20:31:36 +0300

    11 строчка для обработки исключений

--- Cats.py
+++ Cats.py
@@ -1,7 +1,14 @@
+from http.client import responses
 from tkinter import *
 from PIL import Image, ImageTk
 import requests
 from io import BytesIO
+
+
+def load_image():
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
 
 window = Tk()
 window.title("Cats")
