8857caf575bcfaf86ec9674ab2dd83acbdc7c009
YaKaZi85 <aylimak@gmail.com>
2024-11-25 19:41:33 +0300

    Создали метку и путь откуда буудт грузиться файлы

--- Cats.py
+++ Cats.py
@@ -1,1 +1,20 @@
 from tkinter import *
+from PIL import Image, ImageTk
+import requests
+from io import BytesIO
+
+window = Tk()
+window.title("Cats")
+window.geometry("400x600")
+
+label =Label()
+label.pack()
+
+url = ("https://catsaas.com/cat")
+img = load_image(url)
+
+if img:
+    label.config(image=img)
+    label.image = img
+
+window.mainloop()
